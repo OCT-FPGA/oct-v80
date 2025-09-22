deb268536ad48c521d1dcc5b2b9cfe78a7ccdb5b
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2025-09-22 16:27:00 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -7,3 +7,4 @@ sudo apt install /proj/octfpga-PG0/tools/v80/ami_2.3.0-0.0bab29e5.20250915_amd64
 sudo apt install /proj/octfpga-PG0/tools/v80/amd-vrt_1.0.0_2025-09-15-22-20-35_amd64.deb
 #Install the QDMA driver
 sudo apt-get install libaio-dev
+scp -r /proj/octfpga-PG0/tools/v80/qdma_drv /tmp/ && cd /tmp/qdma_drv/QDMA/linux-kernel/ && make && make install
