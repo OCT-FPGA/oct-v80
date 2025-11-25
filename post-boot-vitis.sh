288a602591188bc9eb80f26cbbb322e65cda8dfe
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2025-11-24 19:26:44 -0500

    Update post-boot-vitis.sh

--- post-boot-vitis.sh
+++ post-boot-vitis.sh
@@ -4,7 +4,7 @@ sudo apt install libxml2-dev libzmq3-dev libjsoncpp-dev xvfb -y
 #Install AVED software stack
 sudo apt install /share/tools/v80/vitis-flow/ami/ami_2.3.0-0.0bab29e5.20251021_amd64_22.04.deb
 #Install V80 runtime
-sudo apt install /share/tools/v80/vitis-flow/vrt/amd-vrt_1.0.0_2025-11-24-15-50-46_amd64.deb
+sudo apt install /share/tools/v80/vitis-flow/vrt/amd-vrt_1.0.0_2025-11-24-17-17-30_amd64.deb
 #Install the QDMA driver
 sudo apt-get install libaio-dev
 scp -r /share/tools/v80/vitis-flow/qdma_drv /tmp/ && cd /tmp/qdma_drv/linux-kernel/ && make && sudo make install
