380f59044f6a770ed04f0f5636a83f739475210a
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2025-11-24 22:55:30 -0500

    Update post-boot-vitis.sh

--- post-boot-vitis.sh
+++ post-boot-vitis.sh
@@ -4,10 +4,10 @@ sudo apt install libxml2-dev libzmq3-dev libjsoncpp-dev xvfb -y
 #Install AVED software stack
 sudo apt install /share/tools/v80/vitis-flow/ami/ami_2.3.0-0.0bab29e5.20251021_amd64_22.04.deb
 #Install V80 runtime
-sudo apt install /share/tools/v80/vitis-flow/vrt/amd-vrt_1.0.0_2025-11-24-17-17-30_amd64.deb
+sudo apt install /share/tools/v80/vitis-flow/vrt/amd-vrt_1.0.0_2025-11-25-03-52-43_amd64.deb
 #Install the QDMA driver
 sudo apt-get install libaio-dev
-#scp -r /share/tools/v80/vitis-flow/qdma_drv /tmp/ && cd /tmp/qdma_drv/linux-kernel/ && make && sudo make install
+scp -r /share/tools/v80/vitis-flow/qdma_drv /tmp/ && cd /tmp/qdma_drv/linux-kernel/ && make && sudo make install
 
 #Card preparation
-#sudo ami_tool cfgmem_program -d 0d:00.0 -i /opt/amd/vrt/design.pdi -t primary -p 1 -y
+sudo ami_tool cfgmem_program -d 0d:00.0 -i /opt/amd/vrt/design.pdi -t primary -p 1 -y
