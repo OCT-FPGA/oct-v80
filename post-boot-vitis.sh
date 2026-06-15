0908578f0c8f3f942087fa45c93eecc6ea41b0b3
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2026-06-14 23:39:21 -0400

    Update post-boot-vitis.sh

--- post-boot-vitis.sh
+++ post-boot-vitis.sh
@@ -12,9 +12,22 @@ install_ami_driver(){
     sudo apt install $SLASH_BASE_PATH/deb/slashkit_1.0.0_amd64_*.deb
 }
 
+install_slash_packages(){
+    sudo apt install -y \
+    $SLASH_BASE_PATH/deb/slash-dkms_*_all.deb \
+    $SLASH_BASE_PATH/deb/libslash_*_amd64.deb \
+    $SLASH_BASE_PATH/deb/vrtd_*_amd64.deb \
+    $SLASH_BASE_PATH/deb/libvrtd_*_amd64.deb \
+    $SLASH_BASE_PATH/deb/libvrt_*_amd64.deb \
+    $SLASH_BASE_PATH/deb/v80-smi_*_amd64.deb \
+    $SLASH_BASE_PATH/deb/slashkit_*_amd64.deb
+}
+
 TOOLVERSION=$1
 BASE_DIR="/fpga"
 SLASH_BASE_PATH="$BASE_DIR/tools/v80/$TOOLVERSION"
 mount_filesystems
 install_headers
 install_ami_driver
+install_slash_packages
+#program_board
